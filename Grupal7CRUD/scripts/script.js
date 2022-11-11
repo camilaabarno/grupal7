be2bf3175b323b92fee0ec6669c7e5dfa5f4cb4e
Emanuel Overlay Rodriguez <emanueloverlay@outlook.com>
2022-11-10 22:23:27 -0300

    update2

--- Grupal7CRUD/scripts/script.js
+++ Grupal7CRUD/scripts/script.js
@@ -2,25 +2,30 @@ const inputbuscar = document.getElementById("inputGet1Id");
 const botonBuscar = document.getElementById("btnGet1");
 const results = document.getElementById("results");
 let APIURL = "https://636ac7e5b10125b78fe4a931.mockapi.io/";
-const agregar = document.getElementById("btnPost");
+const btnAgregar = document.getElementById("btnPost");
 const inputNombre = document.getElementById("inputPostNombre");
 const inputApellido = document.getElementById("inputPostApellido");
 const inputPutNombre = document.getElementById("inputPutNombre");
 const inputPutApellido = document.getElementById("inputPutApellido");
-const modificar = document.getElementById("btnPut");
+const btnModificar = document.getElementById("btnPut");
 const inputPutId = document.getElementById("inputPutId");
 const btnSendChanges = document.getElementById("btnSendChanges");
+const btnEliminar = document.getElementById("btnDelete");
+const inputDelete = document.getElementById("inputDelete");
+const errorAlert = document.getElementById("alert-error");
+const modalHTML = document.getElementById("modalHTML");
 
 //BUSCA DATOS EN EL SERVIDOR
 botonBuscar.addEventListener("click", () => {
     mostrarListado();
-    results.innerHTML = ``;
 });
 
 function mostrarListado() {
     fetch(APIURL + "/users").then(function (response) {
         return response.json();
+        debugger;
     }).then(function (data) {
+        results.innerHTML = "";
         for (person of data) {
             if (inputbuscar.value == "") {
                 results.innerHTML += `<p> name: ${person.name}<br>
@@ -36,22 +41,51 @@ function mostrarListado() {
 };
 
 //AGREGA DATOS AL SERVIDOR
-agregar.addEventListener("click", () => {
-    console.log(inputNombre.value);
+btnAgregar.addEventListener("click", () => {
     fetch(APIURL + "/users", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ "name": inputNombre.value, "lastname": inputApellido.value })
     })
         .then(response => response.json())
-        .then(dataResponse => console.log(dataResponse))
+        .then(dataResponse => mostrarListado())
         .catch(error => alert("ocurrióo un error"))
-});
+    inputApellido.value = "";
+    inputNombre.value = "";
+    habilitado();
+}
+);
 
 
 
 //MODIFICA DATOS EN SERVIDOR
-modificar.addEventListener("click", () => {
+// btnModificar.addEventListener("click", () => {
+//     fetch(APIURL + "/users/" + inputPutId.value, {
+//         method: "GET",
+//         headers: { "Content-Type": "application/json" },
+
+//     })
+//         .then(response => response.json())
+//         .then(dataResponse => {
+//             inputPutNombre.value = dataResponse.name;
+//             inputPutApellido.value = dataResponse.lastname;
+//             btnSendChanges.addEventListener("click", () => {
+//                 fetch(APIURL + "/users/" + inputPutId.value, {
+//                     method: "PUT",
+//                     headers: { "Content-Type": "application/json" },
+//                     body: JSON.stringify({ id: inputPutId.value, name: inputPutNombre.value, lastname: inputPutApellido.value })
+//                 })
+//                 mostrarListado();
+//             })
+//         })
+//         .catch(error => alert("ocurrióo un error"))
+//         inputPutId.value = "";
+//         habilitado();
+// });
+
+
+
+btnModificar.addEventListener("click", () => {
     fetch(APIURL + "/users/" + inputPutId.value, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
@@ -67,8 +101,48 @@ modificar.addEventListener("click", () => {
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ id: inputPutId.value, name: inputPutNombre.value, lastname: inputPutApellido.value })
                 })
-                mostrarListado();
+                    .then(response => response.json())
+                    .then(dataResponse => {
+                        mostrarListado()
+                        inputPutId.value = ""; // estas lineas estaban debajo del catch
+                        habilitado();
+                    } )
             })
         })
-        .catch(error => alert("ocurrióo un error"))
+        .catch(error => alert("ocurrióo un error"));
 });
+
+
+
+
+
+
+// ELIMINA REGISTRO DEL SERVIDOR
+btnEliminar.addEventListener("click", () => {
+    var raw = "";
+    var requestOptions = {
+        method: 'DELETE',
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch(APIURL + "/users/" + inputDelete.value, requestOptions)
+        .then((response) => {
+            if (!response.ok) {
+                errorAlert.classList.add("show");
+                setTimeout(() => { errorAlert.classList.remove("show") }, 3000);
+            }
+            mostrarListado()
+        })
+        .catch(error => console.log('error', error));
+    inputDelete.value = "";
+    habilitado();
+
+});
+
+function habilitado() {
+
+    inputNombre.value || inputApellido.value ? btnAgregar.disabled = false : btnAgregar.disabled = true;
+    inputPutId.value ? btnModificar.disabled = false : btnModificar.disabled = true;
+    inputDelete.value ? btnEliminar.disabled = false : btnEliminar.disabled = true;
+}
